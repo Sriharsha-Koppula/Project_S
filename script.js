a095c88fa9755ca813cdb14a4664774cb0768091
Sriharsha-Koppula <sriharsha.koppula96@gmail.com>
2025-06-06 16:33:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function sendMessage(e) {
     const mode = document.body.classList.contains('light') ? 'Light Mode' : 'Dark Mode';
 
     // Send to backend
-    fetch("https://0eec-34-75-75-243.ngrok-free.app/generate", {
+    fetch("https://3e70-34-75-75-243.ngrok-free.app/generate", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ prompt: `(${mode}) ${text}` })
